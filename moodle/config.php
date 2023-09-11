352807cca61f17d5599fe69bcf35c8a729a4f9de
purylte <purylte@gmail.com>
2023-09-11 16:27:36 +0700

    config php use env

--- moodle/config.php
+++ moodle/config.php
@@ -4,21 +4,22 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'mysqli';
+$CFG->dbtype    = getenv('MOODLE_DATABASE_TYPE');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'moodle_db';
-$CFG->dbname    = 'db_moodle';
-$CFG->dbuser    = 'moodle';
-$CFG->dbpass    = 'password';
+$CFG->dbhost    = getenv('MOODLE_DATABASE_HOST');
+$CFG->dbname    = getenv('MOODLE_DATABASE_NAME');
+$CFG->dbuser    = getenv('MOODLE_DATABASE_USER');
+$CFG->dbpass    = getenv('MOODLE_DATABASE_PASSWORD');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => 3306,
+  'dbport' => getenv('MOODLE_DATABASE_PORT_NUMBER'),
   'dbsocket' => '',
   'dbcollation' => 'utf8mb4_unicode_ci',
 );
 
-$CFG->wwwroot   = 'http://localhost:9999';
+$CFG->wwwroot   = getenv('MOODLE_WWW_ROOT');
+
 $CFG->dataroot  = '/bitnami/moodledata';
 $CFG->admin     = 'admin';
 
